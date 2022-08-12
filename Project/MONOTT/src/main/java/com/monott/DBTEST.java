e870e38d4c24b6fb5ec16a62070d042edb976e88
limssoo <ls9585@naver.com>
2022-08-12 11:03:15 +0900

    connect mysql

---
+++ Project/MONOTT/src/main/java/com/monott/DBTEST.java
@@ -0,0 +1,20 @@
+package com.monott;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_test")
+public class DBTEST {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 200, nullable = false)
+    private String memoTextselet;
+}
